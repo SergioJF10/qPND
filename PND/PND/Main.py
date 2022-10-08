a514cb4a5e16ecaff5f769477004f39c16200cc9
SergioJF10 <Sergio.Jimenez19@alu.uclm.es>
2022-10-08 19:08:48 +0200

    Third stage finished
    Some cmd line warnings must be removed yet

--- PND/PND/Main.py
+++ PND/PND/Main.py
@@ -1,9 +1,11 @@
 import matplotlib.pyplot as plt
 import qsharp
+import sys
 from PND import Stage1
 
 CANDIDATE = 11
 GRAPHIC = False
+QUBITS = 4
 
 def array_to_number(array):
     '''Converts an array with bits intepreted as boolean values into integers'''
@@ -14,6 +16,19 @@ def array_to_number(array):
         exponent += 1
 
     return int_res
+
+def stage_0():
+    '''Preprocessing stage filtering for discarding obvius not primes'''
+    if CANDIDATE == 2:
+        return True
+    elif CANDIDATE % 2 == 0:
+        print(f'[PREPROCESS] {CANDIDATE} is NOT a prime number.')
+        return False
+    elif CANDIDATE > (2 ** QUBITS) - 1:
+        print(f'[PREPROCESS] Number {CANDIDATE} cannot be processed with 4 Qubits.')
+        return False
+    else:
+        return True
 
 def stage_1():
     '''First Stage (initialization): Call to Quantum qRAM algorithm for initialization'''
@@ -37,13 +52,45 @@ def stage_2(b_set):
 
     for b1 in b_set:
         for b2 in b_set:
-            c_set.add(b1 * b2)
+            if b1 != 1 and b2 != 1:
+                c_set.add(b1 * b2)
 
     print(f'[STAGE 2] Cartesian product of the B set: \n\t{c_set}')
     return c_set
 
+def stage_3(c_set):
+    '''Third Stage (prime finding): Classic search in a set'''
+    return (CANDIDATE in c_set)
 
-if __name__ == '__main__':
-    print(f'[STAGE 0] Potential prime number a: {CANDIDATE}')
+def main():
+    '''Entry point for the program'''
+    print(f'[STARTING] Potential prime number a: {CANDIDATE}')
+    if not stage_0():
+        return
     b_set = stage_1()
     c_set = stage_2(b_set)
+    if stage_3(c_set):
+        print(f'[STAGE 3] {CANDIDATE} is NOT a prime number.')
+    else:
+        print(f'[STAGE 3] {CANDIDATE} IS a prime number.')
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2 and len(sys.argv) != 3:
+        print('CmdLineError: Potential number not specified')
+        sys.exit(1)
+
+    if (sys.argv[1] == '--plot' or sys.argv[1] == '-p') and len(sys.argv) == 3:
+        GRAPHIC = True
+
+        if sys.argv[2].isdigit():
+            CANDIDATE = int(sys.argv[2])
+            main()
+        else:
+            print('CmdLineError: Potential number not specified')
+
+    elif sys.argv[1].isdigit() and len(sys.argv) == 2:
+        CANDIDATE = int(sys.argv[1])
+        main()
+    else:
+        print('CmdLineError: Potential number not specified')
--- PND/PND/Program.qs
+++ PND/PND/Program.qs
@@ -4,16 +4,8 @@ namespace PND {
     open Microsoft.Quantum.Intrinsic;
     open Microsoft.Quantum.Diagnostics;
     open Microsoft.Quantum.Measurement;
-    
-    //@EntryPoint()
-    operation Sample() : Unit {
-        use reg = Qubit[2];
-        X(reg[1]);
-        DumpMachine();
-        ResetAll(reg);
-    }
 
-    @EntryPoint()
+    //@EntryPoint()
     operation Stage1(shots: Int): Result[][] {
         mutable results = [[Zero], size = shots];
         for i in 0..(shots - 1) {
@@ -33,11 +25,11 @@ namespace PND {
         use data = Qubit[4];
 
         // Boolean encoding of binary conversion for addresses: [0,1,2,3,4] -> [000, 001, 010, 011, 100] -> 0 = false && 1 = true
-        let address_enc = [[false, false, false], [false, false, true], [false, true, false], [false, true, true], [true, false, false]];
-        // Boolean encoding of binary conversion for unused addresses: [5,6,7] -> [101, 110, 111] -> 0 = false && 1 = true
-        let unused_enc = [[true, false, true], [true, true, false], [true, true, true]];
+        let address_enc = [[false, false, false], [false, false, true], [false, true, false], [false, true, true], [true, false, false], [true, false, true], 
+            [true, true, false], [true, true, true]];
         // Boolean encoding of binary conversion for data: [1,3,5,7,9] -> [0001, 0011, 0101, 0111, 1001] -> 0 = false && 1 = true
-        let data_enc = [[false, false, false, true], [false, false, true, true], [false, true, false, true], [false, true, true, true], [true, false, false, true]];
+        let data_enc = [[false, false, false, true], [false, false, true, true], [false, true, false, true], [false, true, true, true], [true, false, false, true],
+            [true, false, true, true], [true, true, false, true], [true, true, true, true]];
 
         //// QRAM ALGORITHM
         for qubit in address {
@@ -47,11 +39,6 @@ namespace PND {
         // qRAM columns
         for i in 0..(Length(address_enc) - 1) {
             (ControlledOnBitString(address_enc[i], EncodeOnBitString))(address, (data, data_enc[i]));
-        }
-        
-        // unwanted addresses conversion
-        for i in 0..(Length(unused_enc) - 1) {
-            (ControlledOnBitString(unused_enc[i], EncodeOnBitString))(address, (data, data_enc[i]));
         }
 
         //// MEASUREMENT
